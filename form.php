283359245a1b855d55559c12fe9f436dd4a70bbd
pnursyifa <62365260+pnursyifa@users.noreply.github.com>
2022-09-29 13:27:05 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Form</title>
+</head>
+<body>
+    <form methods="get" action="">
+        <fieldset>
+            <legend>Form tambah produkkk</legend>
+            <p>
+                <label for="">Nama Produk</label>
+                <input type="text" 
+                value="" 
+                name="nama_produk"/>
+            </p>
+            <p>
+                <label for="">Harga</label>
+                <input type="number" 
+                value="" 
+                name="harga_produk"/>
+            </p>
+            <p>
+                <button type="submit">Save</button>
+            </p>
+        </fieldset>
+    </form>
+</body>
+<?php
+    //print_r($_GET); ini diprint semua sebagai array
+    isset
+?>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $rupiah="1k";
+    $nama = "aku";
+    $umur = 11;
+    echo "halo ini landing page";
+    echo "<br/>";
+    echo $nama;
+    echo "<br/>";
+    echo $umur;
+    echo "<br/>";
+    echo "nama saya ".$nama." dan dia berumur ".$umur;
+    echo "<br/>";
+    echo "nama saya {$nama} dan dia berumur {$umur}";
+    echo "<br/>";
+    echo $rupiah;
+    echo "<br/>";
+    //variable php pake $[nama] case sensitive
+    //float pake titik $something= 11.55555
+    //boolean $something=true / false
+    //$anggota = ["ini", "array"] atau $anggota = array(2, 3, 4), manggilnya $anggota[index]
+    /* */
+?>
